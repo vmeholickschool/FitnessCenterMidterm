32325e015aece5bce1528a033d6e50dced560313
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-03-11 21:20:37 -0400

    Added 4 new classes

---
+++ FitnessCenterMidterm/Club.cs
@@ -0,0 +1,8 @@
+﻿namespace FitnessCenterMidterm
+{
+    internal class Club
+    {
+        public string Address { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ FitnessCenterMidterm/MultiClubMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace FitnessCenterMidterm
+{
+    internal class MultiClubMember : Member
+
+    {
+        public int MembershipPoints {  get; set; }
+
+
+
+        public override void CheckIn(Club club)
+        {
+            int membershipPoints = 0;
+            membershipPoints++;
+            MembershipPoints = membershipPoints;
+        }
+
+        
+    }
+}
+
